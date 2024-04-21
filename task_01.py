554243165057b2832d9240a05c2574b2f20a80a4
Andrew Motko <andrew.motko@gmail.com>
2024-04-21 23:08:40 +0300

    fix: add function check

--- task_01.py
+++ task_01.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 
 def get_days_from_today(date: str) -> int:
-    old_date_obj = datetime.strptime(date, "%Y.%m.%d")
-    current_date = datetime.today()
-    numbers_day = current_date - old_date_obj
-    return numbers_day.days
+    try:
+        old_date_obj = datetime.strptime(date, "%Y.%m.%d")
+        current_date = datetime.today()
+        numbers_day = current_date - old_date_obj
+        return numbers_day.days
+    except ValueError:
+        return "Неправильний формат дати. Будь ласка, введіть дату у форматі 'YYYY.MM.DD'."
 
-print(f"Difference in days is: {get_days_from_today("2020.10.09")}")
+print(f"Difference in days is: {get_days_from_today('2020.10.09')}")
